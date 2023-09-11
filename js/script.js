e72535dac19607c122120ccb2cff5134a958cd3e
Hunter <muratoreh@gmail.com>
2023-09-11 14:16:33 -0400

    Added all question sections to html and styled them in css

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Hide the starting section of the page and display the first question of the quiz while starting the timer.
+function startQuiz() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("question-1").style.display = "flex";
+}
