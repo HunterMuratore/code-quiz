3051eafcd550126e4fe12050d8f51f22b04f9261
Hunter <muratoreh@gmail.com>
2023-09-13 19:45:43 -0400

    Switched to addEventListener to display questions

--- js/script.js
+++ js/script.js
@@ -1,43 +1,77 @@
-// Hide the starting section of the page and display the first question of the quiz while starting the timer.
-function startQuiz() {
-    document.querySelector(".start").style.display = "none";
-    document.querySelector("#start-button").style.display = "none";
-    document.querySelector("#question-1").style.display = "flex";
-    generateAnswers(document.querySelector('#question-1'), answers.question_1_ans)
-    // Start timer
-}
+// GLOBAL VARIABLES
+var start = document.querySelector('.start');
+var questionSection = document.querySelector('.question');
+var end = document.querySelector('#end-quiz');
+var answerResult = document.querySelector('.answer-result');
 
-// Hides the previous section and populates and displays the question/answers for the next section
-function nextQuestion (start, next, result, ans) {
-    ansResult(result)
+var currentQuestionIndex = 0;
 
-    if (start !== '#question-5') {
-        generateAnswers(document.querySelector(next), ans);
-    } 
-
-    document.querySelector('.answer-result').style.display = 'flex';
-    document.querySelector(start).style.display = "none";
-    document.querySelector(next).style.display = "flex";
-}
-
-// Checks their answer to see if it's correct. If wrong subtract 10 seconds from the timer
-function ansResult (result) {
-    if (result === true) {
-        document.querySelector('.answer-result').innerHTML = 'Correct!';
-    } else {
-        document.querySelector('.answer-result').innerHTML = 'Incorrect';
-        // decrease timer by 10 seconds
+// Start the quiz when the user clicks the button by hidding the start section and displaying the question section
+function startQuiz(e) {
+    var el = e.target;
+    if (el.tagName === 'BUTTON') {
+        this.style.display = 'none';
+        nextQuestion(currentQuestionIndex);
     }
 }
 
-// Take the answers held in the answers object and write them into the buttons for each question
-function generateAnswers (question, ans) {
-    let count = 0;
-    question.querySelectorAll('.answer-button').forEach(element => {
-        element.innerText = `${ans[count]}`;
-        count++
+function nextQuestion (index) {
+    // Reset the question section part of the HTML so that the next questions can be appended to it
+    questionSection.innerHTML = '';
+
+    // Create the p element that will contain the question number
+    var questionNum = document.createElement('p');
+    questionNum.innerText = `Question #${index + 1}`;
+    questionNum.classList.add("question-num");
+
+    // Create the p element that will contain the question
+    var questionText = document.createElement('p');
+    questionText.innerText = questions[index];
+    questionText.classList.add("question-text");
+
+    // Display the number and text for the next question
+    questionSection.append(questionNum);
+    questionSection.append(questionText);
+
+    // Gather the list of answers for the current question into a variable so that it can be looped through and assigned to buttons
+    var questionAnswers = answers[`question_${index + 1}_ans`];
+    questionAnswers.forEach(answer => {
+        var btn = document.createElement('button');
+        btn.classList.add('answer-button')
+        btn.innerText = answer;
+        if (answer === correctAnswers[index]) {
+            btn.dataset.correct = 'true';
+        }
+        questionSection.append(btn);
     });
+
+    currentQuestionIndex++;
 }
+
+// Checks their answer to see if it's correct. If wrong subtract 10 seconds from the timer
+function checkAnswer (e) {
+    var el = e.target;
+    // Make sure the user is clicking a button only
+    if (el.tagName === 'BUTTON') {
+        // Display if the user chose the correct or incorrect answer
+        if (el.dataset.correct) {
+            answerResult.innerText = 'Correct!';
+        } else {
+            answerResult.innerText = 'Incorrect';
+            // decrease time 
+        }
+        // Display the next question if there is a next one, otherwise display the end section with the user's score
+        if (currentQuestionIndex < questions.length) {
+            nextQuestion(currentQuestionIndex);
+        } else {
+            questionSection.style.display = 'none';
+            end.style.display = 'flex';
+        }
+    }
+}
+
+start.addEventListener('click', startQuiz);
+questionSection.addEventListener('click', checkAnswer);
 
 // Object to hold all of the answers to the questions
 var answers = {
@@ -47,3 +81,21 @@ var answers = {
     question_4_ans: ['1. Is this the Answer?', '2. Or is this the Answer?', '3. Maybe this is the Answer?', '4. Wait I think this might be the Answer?'],
     question_5_ans: ['1. Is this the Answer?', '2. Or is this the Answer?', '3. Maybe this is the Answer?', '4. Wait I think this might be the Answer?'],
 }
+
+// Array to hold all of the questions
+var questions = [
+    'This is question #1, answer it please.',
+    'This is question #2, answer it please.',
+    'This is question #3, answer it please.',
+    'This is question #4, answer it please.',
+    'This is question #5, answer it please.',
+];
+
+// Array to hold all of the correct answers
+var correctAnswers = [
+    '1. Is this the Answer?', 
+    '3. Maybe this is the Answer?',
+    '4. Wait I think this might be the Answer?',
+    '2. Or is this the Answer?',
+    '1. Is this the Answer?'
+];
