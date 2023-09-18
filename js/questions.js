56a6370921fae9ddd964a4c3beaad48e682cd47d
Hunter <muratoreh@gmail.com>
2023-09-18 09:49:43 -0400

    moved questions to questions.js

---
+++ js/questions.js
@@ -0,0 +1,26 @@
+// Object to hold all of the answers to the questions
+var answers = {
+    question_1_ans: ['1. By using the <js> tag', '2. By using the <script> tag with the src attribute pointing to the JavaScript file', '3. By using the <link> tag with the rel attribute set to "javascript"', '4.By using the <a> tag with the href attribute pointing to the JavaScript file'],
+    question_2_ans: ['1. <!-- This is a comment -->', '2.  /* This is a comment */', '3. // This is a comment', '4.  \' This is a comment \''],
+    question_3_ans: ['1. To loop through a block of code', '2.  To declare a function', '3. To execute code conditionally based on a specified condition', '4. To define an object'],
+    question_4_ans: ['1. Styling web pages', '2. Adding interactivity to web pages', '3. Creating web page layouts', '4. Optimizing website performance'],
+    question_5_ans: ['1.  ===', '2. ==', '3. =>', '4. !=='],
+}
+
+// Array to hold all of the questions
+var questions = [
+    'How do you link an external JavaScript file to an HTML document?',
+    'Which of the following is used to comment out a single line of JavaScript code?',
+    'What is the purpose of the if statement in JavaScript?',
+    'What is JavaScript primarily used for in web development?',
+    'Which operator is used for strict equality (both value and data type must match) in JavaScript?',
+];
+
+// Array to hold all of the correct answers
+var correctAnswers = [
+    '2. By using the <script> tag with the src attribute pointing to the JavaScript file', 
+    '3. // This is a comment',
+    '3. To execute code conditionally based on a specified condition',
+    '2. Adding interactivity to web pages',
+    '1.  ==='
+];
